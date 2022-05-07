2538d70a107d90068810c7565feb320dfba24542
denismitr <denis.mitr@gmail.com>
2022-05-07 20:16:18 +0300

    adds node grow and shrink tests, plus a Makefile

--- node_test.go
+++ node_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+var (
+	emptyKey = []byte("")
+)
+
 func TestNode4_AddChild_PreserveSorted(t *testing.T) {
 	n := newNode4[string]().inner
 
@@ -38,6 +42,43 @@ func TestNode16_AddChild4_PreserveSort(t *testing.T) {
 		got := n.keys[i]
 		if want != got {
 			t.Errorf("unexpected key sequence at index %d: want=%+v, got=%+v", i, want, got)
+		}
+	}
+}
+
+func TestGrow(t *testing.T) {
+	nodes := []*Node[string]{newNode4[string](), newNode16[string](), newNode48[string]()}
+	expectedKinds := []nodeKind{node16, node48, node256}
+
+	for i, n := range nodes {
+		n.inner.grow()
+		want := expectedKinds[i]
+		got := n.kind()
+		if want != got {
+			t.Errorf("Unexpected node kind at index %d after growing: want=%+v, got=%+v", i, want, got)
+		}
+	}
+}
+
+func TestShrink(t *testing.T) {
+	nodes := []*Node[string]{newNode256[string](), newNode48[string](), newNode16[string](), newNode4[string]()}
+	expectedKinds := []nodeKind{node48, node16, node4, leaf}
+
+	for i, n := range nodes {
+		in := n.inner
+		for j := 0; j < in.minSize(); j++ {
+			if n.kind() != node4 {
+				in.addChild(byte(i), newNode4[string]())
+			} else {
+				in.addChild(byte(i), newLeafNode[string](emptyKey, ""))
+			}
+		}
+
+		n.shrink()
+		want := expectedKinds[i]
+		got := n.kind()
+		if want != got {
+			t.Errorf("Unexpected node kind at index %d after shrinking: want=%+v, got=%+v", i, want, got)
 		}
 	}
 }
